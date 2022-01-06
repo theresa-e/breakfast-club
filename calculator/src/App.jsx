44b4a95a6f031899046506b5ec33d5741fd4ce4a
Theresa Eliger <910theresa@gmail.com>
2022-01-06 12:09:51 -0800

    initial commit

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -5,7 +5,16 @@ function App() {
 
   return (
     <div className="App">
-
+      <div className="wrapper">
+        <div className="calculator">
+          <div className="circles">
+            <div className="red"></div>
+            <div className="yellow"></div>
+            <div className="green"></div>
+          </div>
+          
+        </div>
+      </div>
     </div>
   )
 }
